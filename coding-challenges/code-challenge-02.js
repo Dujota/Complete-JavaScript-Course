a20ed5af73ceb418564df9aa12cbaff113544fb9
Denis <ddujota@internationalsafety.com>
2018-07-16 13:04:08 -0400

    finished the code challenge, traversed through the object and saved the averages into an array and then checked the larges through a reduce function and then check for the index of the winner in the average array so i can properly display the message :

--- coding-challenges/code-challenge-02.js
+++ coding-challenges/code-challenge-02.js
@@ -1,11 +1,48 @@
-data = [
+const data = [
   {
-    john: [89, 120, 103]
+    name: 'John',
+    games: [116, 94, 123]
   },
   {
-    mike: [116, 94, 123]
+    name: 'Mike',
+    games: [89, 120, 103]
   },
   {
-    mary: [97, 134, 105]
+    name: 'Mary',
+    games: [97, 134, 105]
   }
 ];
+
+let winner;
+let averageScores = [];
+
+const average = data => {
+  data.forEach((team, index) => {
+    // calcularte the average
+    team.games.forEach(item => {
+      let total = 0;
+      total += item;
+      return (team.average = (total / team.games.length).toPrecision(4));
+    });
+
+    // find the largest average
+    averageScores.push(team.average);
+    averageScores.reduce((previousValue, currentValue) => {
+      return previousValue > currentValue
+        ? (winner = previousValue)
+        : (winner = currentValue);
+    }, 0);
+  });
+};
+
+const showWinner = (data, averageScores) => {
+  average(data);
+  winnerIndex = averageScores.indexOf(winner);
+  console.log(
+    `The winning team is ${
+      data[winnerIndex].name
+    } with an average team score of ${winner}`
+  );
+};
+
+showWinner(data, averageScores);
